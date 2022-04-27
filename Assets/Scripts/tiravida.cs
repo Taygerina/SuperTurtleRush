072857b7f12626bd1815c528d8c802e9658c1029
Talita <talitamarcelo15@gmail.com>
2022-04-27 14:18:46 -0300

    Animação iddle e tira vida
    Adicionei um codigo de gameover e animacoes na turtle

--- Assets/Scripts/tiravida.cs
+++ Assets/Scripts/tiravida.cs
@@ -1,26 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class tiravida : MonoBehaviour
 {
-    public int damage = 1;
-    public GameObject fx;
-
-    public UIManager uimanager;
-
-    private void Start()
-    {
-        uimanager = FindObjectOfType<UIManager>();
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            uimanager.SetSLives(damage);
-            Instantiate(fx, transform.position, Quaternion.identity);
+            other.gameObject.GetComponent<VidaTurtle>().ReceberDano();
 
             Destroy(gameObject);
         }
